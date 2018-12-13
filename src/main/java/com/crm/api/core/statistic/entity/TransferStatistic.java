72006177e55297e9bcb87c79ffaebe1b80de17fa
Rab <133313829@qq.com>
2018-12-13 11:31:15 +0800

    添加微信属性

--- src/main/java/com/crm/api/core/statistic/entity/TransferStatistic.java
+++ src/main/java/com/crm/api/core/statistic/entity/TransferStatistic.java
@@ -18,4 +18,10 @@ public class TransferStatistic{
     private String date;
     //微信ID
     private String wechatId;
+    //微信名
+    private String nickname;
+    //微信号
+    private String wxno;
+    //头像
+    private String headImgUrl;
 }
